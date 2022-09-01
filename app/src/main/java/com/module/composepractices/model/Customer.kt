d827872d06592260492907452bb86c5f4d1535ce
Rajendhiraneasu <rajendhirandev@gmail.com>
2022-09-01 19:40:56 +0530

    -- updated customer screen with state

--- app/src/main/java/com/module/composepractices/model/Customer.kt
+++ app/src/main/java/com/module/composepractices/model/Customer.kt
@@ -1,3 +1,22 @@
 package com.module.composepractices.model
 
-data class Customer(val id: Int, val name: String, val desc:String="")
+import kotlin.random.Random
+
+data class Customer(val id: Int, val name: String, val desc: String = "") {
+    var isSelected: Boolean = false
+}
+
+fun getMyCustomer() = MutableList(60) {
+    Customer(it, "Employee ${it + 1}", getMyTagLine()[Random.nextInt(getMyTagLine().size)])
+}
+
+fun getMyTagLine() =
+    listOf(
+        "Engineer Manager",
+        "Engineering Director",
+        "Sr. Director",
+        "Sr. Developer",
+        "Lead",
+        "Developer",
+        "Architect"
+    )
--- app/src/main/java/com/module/composepractices/screens/CustomerList.kt
+++ app/src/main/java/com/module/composepractices/screens/CustomerList.kt
@@ -1,102 +1,156 @@
 package com.module.composepractices.screens
 
+import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Divider
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.module.composepractices.R
 import com.module.composepractices.model.Customer
+import com.module.composepractices.model.getMyCustomer
 import com.module.composepractices.ui.theme.ComposePracticesTheme
-import kotlin.random.Random
 
 @Composable
 fun MyCustomers() {
     ComposePracticesTheme {
-        MyCustomerListStateful()
+        Log.d("TestCompose", "MyCustomerListStateful")
+        val customerList = remember {
+            getMyCustomer().toMutableStateList()
+        }
+        MyCustomerList(
+            modifier = Modifier,
+            customerList = customerList
+        ) { customerList.remove(it) }
     }
 }
 
 @Composable
-fun MyCustomerListStateful() {
-    val ctx = LocalContext.current
-    MyCustomerListStateless(modifier = Modifier, customerList = getMyCustomer()) {
-        Toast.makeText(ctx, "${it.name} is ${it.desc} - clicked", Toast.LENGTH_SHORT).show()
-    }
-}
-
-fun getMyCustomer() = MutableList(60) {
-    Customer(it, "Employee ${it + 1}", getMyTagLine()[Random.nextInt(getMyTagLine().size)])
-}
-
-fun getMyTagLine() =
-    listOf(
-        "Engineer Manager",
-        "Engineering Director",
-        "Sr. Director",
-        "Sr. Developer",
-        "Lead",
-        "Developer",
-        "Architect"
-    )
-
-@Composable
-fun MyCustomerListStateless(
+fun MyCustomerList(
     modifier: Modifier,
     customerList: List<Customer>,
-    clickedItem: (Customer) -> Unit
+    removeCustomer: (Customer) -> Unit
 ) {
+    val ctx = LocalContext.current
+    Log.d("TestCompose", "MyCustomerListStateless")
     LazyColumn(
         modifier = modifier
-            .padding(5.dp)
             .fillMaxWidth()
             .fillMaxHeight()
     ) {
         items(customerList, key = { it.id }) {
+            Log.d("TestCompose", "MyCustomerListStateless-Items")
             CustomerListItem(
                 it,
-                modifier = modifier,
-                clicked = { clickedItem(it) })
+                modifier = modifier, {
+                    val c = customerList.count { it.isSelected }
+                    if (c > 0) {
+                        Toast.makeText(ctx, "Total $c Employee(s) Selected ", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }, { removeCustomer(it) })
         }
     }
 }
 
+//Stateful Compose
 @Composable
-fun CustomerListItem(customer: Customer, modifier: Modifier, clicked: () -> Unit) {
+fun CustomerListItem(
+    customer: Customer,
+    modifier: Modifier,
+    onCountUpdates: () -> Unit,
+    onRemove: () -> Unit
+) {
+    val ctx = LocalContext.current
+
+    Log.d("TestCompose", "CustomerListItem-Stateful")
+    var isChecked by rememberSaveable {
+        mutableStateOf(false)
+    }
+    //val onCheckChange: (Boolean) -> Unit = { newCheck -> isChecked = newCheck }
+    CustomerListItem(customer = customer, modifier = modifier, {
+        Toast.makeText(ctx, "${customer.name} is ${customer.desc} clicked", Toast.LENGTH_SHORT)
+            .show()
+    }, isChecked, {
+        customer.isSelected = it
+        isChecked = it
+        if (it) {
+            onCountUpdates()
+        }
+    }, { onRemove() })
+}
+
+// Stateless Compose
+@Composable
+fun CustomerListItem(
+    customer: Customer,
+    modifier: Modifier,
+    clickedItem: () -> Unit,
+    isChecked: Boolean,
+    onCheckChange: (Boolean) -> Unit,
+    onRemove: () -> Unit
+) {
+    Log.d("TestCompose", "CustomerListItem-Stateless")
     val commonModifier = modifier.fillMaxWidth()
-    Column(modifier = commonModifier
-        .clickable { clicked() }) {
-        Text(
-            text = customer.name,
-            modifier = commonModifier
-                .padding(15.dp, 15.dp, 15.dp, 0.dp),
-            colorResource(id = R.color.purple_700),
-            style = TextStyle(fontSize = 24.sp),
-            maxLines = 1,
-            overflow = TextOverflow.Ellipsis
-        )
-        Text(
-            text = customer.desc,
-            modifier = commonModifier
-                .padding(15.dp, 0.dp, 15.dp, 15.dp),
-            colorResource(id = R.color.black),
-            style = TextStyle(fontSize = 16.sp),
-            maxLines = 1,
-            overflow = TextOverflow.Ellipsis
-        )
+    Surface(modifier = commonModifier) {
+        Log.d("TestCompose", "CustomerListItem-Stateless-Surface")
+        Row(
+            modifier = commonModifier.clickable { clickedItem() },
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Log.d("TestCompose", "CustomerListItem-Stateless-Row")
+            Column(
+                modifier = commonModifier
+                    .weight(1f)
+            ) {
+                Log.d("TestCompose", "CustomerListItem-Stateless-Column")
+                Text(
+                    text = customer.name,
+                    modifier = commonModifier
+                        .padding(15.dp, 15.dp, 15.dp, 0.dp),
+                    colorResource(id = R.color.purple_700),
+                    style = TextStyle(fontSize = 24.sp),
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = customer.desc,
+                    modifier = commonModifier
+                        .padding(15.dp, 0.dp, 15.dp, 15.dp),
+                    colorResource(id = R.color.black),
+                    style = TextStyle(fontSize = 16.sp),
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+            Log.d("TestCompose", "CustomerListItem-Stateless-Checkbox")
+            Checkbox(checked = isChecked, onCheckedChange = onCheckChange)
+
+            IconButton(onClick = { onRemove() }) {
+                Icon(Icons.Filled.Close, contentDescription = "Close")
+            }
+        }
         Divider()
     }
+}
+
+@Preview
+@Composable
+fun CustomerPreview() {
+    MyCustomers()
 }
